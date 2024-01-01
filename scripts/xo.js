c1dc42f2d48c84dbeb1c7526688e00cafb7b639d
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2024-01-01 16:08:22 +0000

    changed xo.js

--- scripts/xo.js
+++ scripts/xo.js
@@ -3,3 +3,7 @@ const server = io('https://profound-sundae-898452.netlify.app:3000/')
 server.on('connect', ()=>{
     console.log(server.id)
 })
+
+server.on('list_users',(e)=>{
+    console.log(e)
+})
